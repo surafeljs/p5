f4192785d87144ba7b99910f76a3c5057343f18a
surafeljs <surafel.node@gmail.com>
2025-07-21 15:20:00 -0700

    ellipse

--- sketch.js
+++ sketch.js
@@ -4,11 +4,41 @@ function setup() {
 
 function draw() {
   background(220);
+
+  ellipse(70, 70, 90, 90);
+  ellipse(70, 80, 90, 90);
+  ellipse(70, 90, 90, 90);
+  ellipse(70, 100, 90, 90);
+  ellipse(70, 110, 90, 90);
+  ellipse(70, 120, 90, 90);
+  ellipse(70, 130, 90, 90);
+  ellipse(70, 140, 90, 90);
+  ellipse(70, 150, 90, 90);
+  ellipse(70, 140, 90, 90);
+  ellipse(70, 150, 90, 90);
+  ellipse(70, 160, 90, 90);
+  ellipse(70, 170, 90, 90);
+  ellipse(80, 170, 90, 90);
+  ellipse(90, 170, 90, 90);
+  ellipse(100, 170, 90, 90);
+  ellipse(120, 170, 90, 90);
+  ellipse(130, 170, 90, 90);
+  ellipse(130, 170, 90, 90);
+  ellipse(140, 170, 90, 90);
+  ellipse(150, 170, 90, 90);
+  ellipse(170, 170, 90, 90);
+  ellipse(160, 170, 90, 90);
+  ellipse(170, 170, 90, 90);
+
   ellipse(180, 180, 90, 90);
   ellipse(190, 190, 90, 90);
   ellipse(200, 200, 90, 90);
   ellipse(210, 210, 90, 90);
   ellipse(220, 220, 90, 90);
+  ellipse(230, 230, 90, 90);
+  ellipse(240, 240, 90, 90);
+  ellipse(240, 240, 90, 90);
+  ellipse(260, 260, 90, 90);
 
   stroke("green");
   fill("red");
