055ec137c4a89e9010dfb7f6f647bd277c5e3180
surafeljs <surafel.node@gmail.com>
2025-07-21 16:01:23 -0700

    rec_ellipse

---
+++ rec.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // background(random(255), random(255), random(255), random(255));
+
+  background("pink");
+  rect(50, 50, 150, 100);
+  rect(60, 60, 150, 100);
+  rect(70, 70, 150, 100);
+  ellipse(180, 150, 100, 100);
+  ellipse(190, 160, 100, 100);
+  ellipse(200, 170, 100, 100);
+
+  fill("yellow");
+}
