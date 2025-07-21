aa06a443b0712bed9211d3f15640bcc55a7c3e71
surafeljs <surafel.node@gmail.com>
2025-07-21 15:11:57 -0700

    firstcommit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(180, 180, 90, 90);
+  ellipse(190, 190, 90, 90);
+  ellipse(200, 200, 90, 90);
+  ellipse(210, 210, 90, 90);
+  ellipse(220, 220, 90, 90);
+
+  stroke("green");
+  fill("red");
+  noFill();
+}
